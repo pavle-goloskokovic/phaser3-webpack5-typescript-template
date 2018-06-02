18739c9b548197ca70e2ee3e0273a9d994248031
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2018-06-02 22:59:07 +0200

    webpack

---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+import { resolve, join } from "path";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const pathToPhaser = join(__dirname, '/node_modules/phaser/');
+const phaser = join(pathToPhaser, 'dist/phaser.js');
+
+import * as appConfig  from './src/ts/app.config';
+
+module.exports = {
+    entry: resolve(__dirname, 'src', 'ts', 'app.ts'),
+    output: {
+        filename: '[name].js',
+        path: resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.pug?$/,
+                loader: 'pug-loader',
+                query: { pretty: true }
+            },
+            {
+                test: /phaser\.js$/,
+                loader: 'expose-loader?Phaser'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+        alias: {
+            phaser: phaser
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: appConfig.title,
+            template: './src/templates/index.pug',
+            data: {
+                description: appConfig.description
+            }
+        })
+    ]
+};
