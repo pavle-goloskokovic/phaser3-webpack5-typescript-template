3f6354fd3d996b1a44b1709b725291766508be9d
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2019-09-22 18:57:17 +0200

    making PhaserStatsGame class a bit more generic

--- src/ts/classes/PhaserStatsGame.ts
+++ src/ts/classes/PhaserStatsGame.ts
@@ -7,22 +7,22 @@ export default class PhaserStatsGame extends Phaser.Game {
 
     private stats: Stats;
 
-    constructor (config: Phaser.Types.Core.GameConfig)
+    constructor (config?: Phaser.Types.Core.GameConfig)
     {
         super(config);
 
         this.stats = new Stats();
         this.stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-        document.getElementById('container').appendChild(this.stats.dom);
 
-        this.stats.dom.style.cssText = `
-            position: fixed;
-            bottom: 0;
-            right: 0;
-            cursor: pointer;
-            opacity: 0.9;
-            z-index: 10000;
-        `;
+        this.stats.dom.style.top = this.stats.dom.style.left = '';
+        this.stats.dom.style.bottom = this.stats.dom.style.right = '0';
+    }
+
+    protected boot (): void
+    {
+        super.boot();
+
+        this.canvas.parentNode.appendChild(this.stats.dom);
     }
 
     step (time: integer, delta: number): void
