d66af220ad084e8c3d4d941eb897acffc287a959
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2024-01-31 16:00:28 +0100

    few config changes

--- src/ts/game.config.ts
+++ src/ts/game.config.ts
@@ -1,37 +1,38 @@
-/**
- * Game title used for page title tag and metadata.
- * @type {string}
- */
-export const title =
-    'Phaser 3.70, Webpack 5, and TypeScript template'; // TODO update
-/**
- * Game description used for html page metadata.
- * @type {string}
- */
-export const description = 'TODO add description'; // TODO update
-/**
- * Setting which enables us to quickly mute game sounds.
- * @type {boolean}
- */
-export const mute = false;
-/**
- * Setting which determines if stats should be enabled in game.
- * @type {boolean}
- */
-export const stats = true;
-/**
- * Game dimensions
- * @type {{w: number; h: number}}
- */
-export const size: {
-    readonly w: number;
-    readonly h: number;
-} = {
-    w: 1070,
-    h: 600
-};
-/**
- * Google Analytics 4 tag ID
- * @type {string}
- */
-export const tagId: string = null; // 'TAG_ID'; // TODO update
+export const
+    /**
+     * Game title used for page title tag and metadata.
+     * @type {string}
+     */
+    title =
+        'Phaser 3.70, Webpack 5, and TypeScript template', // TODO update
+    /**
+     * Game description used for html page metadata.
+     * @type {string}
+     */
+    description = 'TODO add description', // TODO update
+    /**
+     * Setting which enables us to quickly mute game sounds.
+     * @type {boolean}
+     */
+    mute = false,
+    /**
+     * Setting which determines if stats should be enabled in game.
+     * @type {boolean}
+     */
+    stats = true,
+    /**
+     * Game dimensions
+     * @type {{w: number; h: number}}
+     */
+    size: {
+        readonly w: number;
+        readonly h: number;
+    } = {
+        w: 1070,
+        h: 600
+    },
+    /**
+     * Google Analytics 4 tag ID
+     * @type {string}
+     */
+    tagId: string = null; // 'TAG_ID'; // TODO update
